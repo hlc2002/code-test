055718419fa1180e3686f81e6794ccaf3de5ee0e
forestSpringH <huanglinchun@runjingchina.com>
2023-07-12 18:33:51 +0800

    酒世界门店名称简化方法

---
+++ src/main/java/com/runjing/learn_runjing/utils/AddressAndCodeFiltration.java
@@ -0,0 +1,50 @@
+package com.runjing.learn_runjing.utils;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author forestSpringH
+ * @date 2023/7/12
+ * @project learn_runjing
+ */
+@Slf4j
+public class AddressAndCodeFiltration {
+    private final static String SHI = "市";
+    private final static String XIAN = "县";
+    private final static String QU = "区";
+    private final static String ZHOU = "州";
+    private final static String JSJ = "京东酒世界";
+
+    public static String getResultString(String workHouseNameAndCode) {
+        if (StringUtils.isEmpty(workHouseNameAndCode) || StringUtils.isBlank(workHouseNameAndCode)) {
+            log.error("入参workHouseNameAndCode:{}", workHouseNameAndCode);
+            throw new RuntimeException("入参workHouseNameAndCode不符合处理格式！");
+        } else {
+            String trimHandle = workHouseNameAndCode.replace(" ","");
+            if ((trimHandle.contains(SHI) && (trimHandle.contains(XIAN) || trimHandle.contains(QU))) || (trimHandle.contains(ZHOU))) {
+                int i = 0;
+                if (trimHandle.contains(SHI)) {
+                    i = trimHandle.indexOf(SHI);
+                } else {
+                    i = trimHandle.indexOf(ZHOU);
+                }
+                return trimHandle.substring(i + 1);
+            }
+            return trimHandle.replace(JSJ, "");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getResultString("北京市朝阳区BJS000001京东酒世界"));
+        System.out.println(getResultString("北京朝阳区BJS000001京东酒世界"));
+        System.out.println(getResultString("北京市朝阳县BJS000001京东酒世界"));
+        System.out.println(getResultString("北京州BJS000001京东酒世界"));
+        System.out.println(getResultString("BJS000001京东酒世界"));
+        System.out.println(getResultString("     BJS000001京东酒世界   "));
+        System.out.println(getResultString("BJS000001   京  东酒  世界"));
+        System.out.println(getResultString("        "));
+        System.out.println(getResultString("  ss   sss    "));
+    }
+}
