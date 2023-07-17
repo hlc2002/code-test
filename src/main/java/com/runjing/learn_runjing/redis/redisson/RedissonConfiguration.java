84b36f9c71374c1f812d59ae68e93f040c4ba676
forestSpringH <huanglinchun@runjingchina.com>
2023-07-17 15:51:56 +0800

    redisson地址配置

--- src/main/java/com/runjing/learn_runjing/redis/redisson/RedissonConfiguration.java
+++ src/main/java/com/runjing/learn_runjing/redis/redisson/RedissonConfiguration.java
@@ -2,6 +2,7 @@ package com.runjing.learn_runjing.redis.redisson;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -19,12 +20,16 @@ public class RedissonConfiguration {
 
     @Value("${spring.data.redis.host}")
     private String redisHost;
-
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.password}")
+    private String password;
     @Bean(destroyMethod = "shutdown",name = "redisson")
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public RedissonClient redisson() {
         Config config = new Config();
-        config.useSingleServer().setAddress(redisHost);
+        config.useSingleServer().setAddress("redis://"+redisHost+":"+port).setPassword(password);
+        config.setCodec(new JsonJacksonCodec());
         return Redisson.create(config);
     }
 }
